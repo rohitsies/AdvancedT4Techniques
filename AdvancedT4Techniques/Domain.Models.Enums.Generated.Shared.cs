b0bb73fce6b0fdd20a7903437bb1e2dda0b830d6
Rohit Patel <rohitsies@gmail.com>
2019-06-14 16:56:37 +1000

    Initial Commit

---
+++ AdvancedT4Techniques/Domain.Models.Enums.Generated.Shared.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models.Enums.Generated.Shared
+{
+    public enum ProductStatus:int
+	{
+		[EnumMember]
+		[Display(Name = "One")]
+		One= 1,
+
+		[EnumMember]
+		[Display(Name = "Two")]
+		Two= 2,
+
+		[EnumMember]
+		[Display(Name = "Three")]
+		Three= 3
+	}
+}
